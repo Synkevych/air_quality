75ec2adff445c84365fc72194df678bcef8016b0
Synkevych Roman <sunkevu4@gmail.com>
2021-12-25 11:46:14 +0200

    update multilevel data manager

--- multi_level_data_manager.py
+++ multi_level_data_manager.py
@@ -1,63 +1,72 @@
 import subprocess, cdsapi, os
 from datetime import datetime, timedelta
 
-path = os.getcwd() + "/data/"
+path_to_data = os.getcwd() + "/data"
 
-isExist = os.path.exists(path)
+def prev_file_exist(file_name):
+   is_file_exist = os.path.exists(path_to_data + file_name)
 
-if not isExist:
-  os.makedirs(path)
-  print("The new directory \"data\" is created!")
+   if not is_file_exist:
+      formatted_date = datetime.now().replace(hour=int(get_downloaded_time())).strftime("%Y.%m.%d-%H:00")
+      file_name = "multi_level_" + formatted_date + ".netcdf_zip"
+      download_new_file(file_name)
 
-def get_downloaded_time(date):
-    delay = 3 # in hours from cds
+def create_data_dir():
+   # create data folder if not exist
+   is_data_exist = os.path.exists(path_to_data)
+
+   if not is_data_exist:
+      os.makedirs(path_to_data)
+      print("The new directory \"data\" created!")
+
+def get_downloaded_time():
+    # in hours from cds api recomendation
+    date = datetime.utcnow()
+    delay = 3
     if (date.hour - delay) >= 12:
         return date.replace(hour=12).strftime("%H")
     else:
         return date.replace(hour=0).strftime("%H")
 
-period = datetime.now().strftime("%Y-%m-%d/%Y-%m-%d")
-downloaded_time = get_downloaded_time(datetime.utcnow())
+def download_new_file(file_name):
+   period = datetime.now().strftime("%Y-%m-%d/%Y-%m-%d")
 
-formatted_date = datetime.now().replace(hour=int(downloaded_time), minute=0).strftime("%Y.%m.%d-%H:%M")
-file_name = "multi_level_" + formatted_date + ".netcdf_zip"
+   create_data_dir()
+   c = cdsapi.Client()
 
-c = cdsapi.Client()
-
-c.retrieve(
-    'cams-global-atmospheric-composition-forecasts',
-    {
-        'date': period,
-        'type': 'forecast',
-        'format': 'grib',
-        'variable': [
-            'ammonium_aerosol_mass_mixing_ratio', 'formaldehyde',
-            'nitrogen_dioxide', 'nitric_acid', 'nitrogen_monoxide',
-            'ozone', 'sulphur_dioxide',
-        ],
-        'time': downloaded_time + ":00",
-        'leadtime_hour': [
-            '0', '102', '105',
-            '108', '111', '114',
-            '117', '12', '120',
-            '15', '18', '21',
-            '24', '27', '3',
-            '30', '33', '36',
-            '39', '42', '45',
-            '48', '51', '54',
-            '57', '6', '60',
-            '63', '66', '69',
-            '72', '75', '78',
-            '81', '84', '87',
-            '9', '90', '93',
-            '96', '99',
-        ],
-        'model_level': '137',
-    },
-    path + file_name)
-
-print("file " + file_name + " successfully saved.")
+   c.retrieve(
+       'cams-global-atmospheric-composition-forecasts',
+       {
+           'date': period,
+           'type': 'forecast',
+           'format': 'grib',
+           'variable': [
+               'ammonium_aerosol_mass_mixing_ratio', 'formaldehyde',
+               'nitrogen_dioxide', 'nitric_acid', 'nitrogen_monoxide',
+               'ozone', 'sulphur_dioxide',
+           ],
+           'time': get_downloaded_time() + ":00",
+           'leadtime_hour': [
+               '0', '102', '105',
+               '108', '111', '114',
+               '117', '12', '120',
+               '15', '18', '21',
+               '24', '27', '3',
+               '30', '33', '36',
+               '39', '42', '45',
+               '48', '51', '54',
+               '57', '6', '60',
+               '63', '66', '69',
+               '72', '75', '78',
+               '81', '84', '87',
+               '9', '90', '93',
+               '96', '99',
+           ],
+           'model_level': '137',
+       },
+       path_to_data + file_name)
+   print("File " + file_name + " successfully saved.")
 
 file_object = open('download_log.txt', 'a')
-file_object.write('New download at ' + datetime.now().strftime("%d.%m.%Y-%H:%M") + '\n')
+file_object.write('New download at ' + datetime.now().strftime("%d.%m.%Y-%H:%M:%S") + '\n')
 file_object.close()
