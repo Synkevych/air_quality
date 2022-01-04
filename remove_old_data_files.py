d7fd53e46bf4729608c6443475317e7e764cff7a
Roman <sunkevu4@gmail.com>
2022-01-04 15:10:43 +0200

    updated file removes script

--- remove_old_data_files.py
+++ remove_old_data_files.py
@@ -3,8 +3,8 @@
 import os
 from datetime import datetime, timedelta
 
-days_interval=14
-files_path = os.getcwd() + "/data/"
+days_interval=6
+files_path = "/home/user_name/air_quality/data/"
 os.chdir(files_path)
 
 for f in os.listdir(files_path):
@@ -13,4 +13,4 @@ for f in os.listdir(files_path):
       if os.path.isfile(f):
         os.remove(os.path.join(files_path, f))
 
-print("files that older that "+str(days_interval)+" days removed")
+print("files that older that " + str(days_interval) + " days removed")
